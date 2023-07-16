4edc5da75c8243eff4cc3e1f5594c04e4a3f3803
jhimi17 <jhimimarcelo17@hotmail.com>
2023-07-16 16:04:07 -0500

    ejercio 8 resuelto

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+
+a = int(input("ingresa la edad de la primera persona"))
+b = int(input("ingresa la edad de la segunda persona"))
+
+if a>b:
+    print("primera persona ",a," segunda persona ",b," la diferencia de edad es de : ",a-b)
+
+else:
+     print("primera persona ",a," segunda persona ",b," la diferencia de edad es de : ",b-a)
