f4744d165580a082db9f71d1d792ef8f7b9fdc3d
KevinSanchez <kevinsanchezcabrerakevin@gmail.com>
2023-07-16 20:44:56 -0500

    ejercicio 11 resuelto

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,6 +1,15 @@
-#numero diferente a 0 e indicar si es par o impar
-numero = int (input("Ingrese un numero"))
-if numero % 2 == 0:
-    print("El numero es par: ")
-else :
-    print("El numero es impar: ")
+#numero entero y diferente a 0 e indicar si es par o impar
+numero = 0
+
+while numero == 0:
+    try:
+        numero = int(input("Ingrese un número diferente de cero: "))
+        if numero == 0:
+            print("El número debe ser diferente de cero. Inténtelo nuevamente.")
+    except ValueError:
+        print("Error: Debe ingresar un número entero. Inténtelo nuevamente.")
+
+if numero %2 == 0:
+    print("El número ", numero, " es par")
+else:
+    print("El número ", numero, " es impar")
