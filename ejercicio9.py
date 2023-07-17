0cc3d31c6802190a1d8a6cfb6f5d3b88c9204ae7
KevinSanchez <kevinsanchezcabrerakevin@gmail.com>
2023-07-16 20:09:43 -0500

    ejercicio 9  resuelto

---
+++ ejercicio9.py
@@ -0,0 +1,16 @@
+#20 edades y decir cuales son mayores y menores de edad
+import random
+
+edades = []
+
+for _ in range(20):
+    edad = random.randint(1, 100) 
+    edades.append(edad)
+
+print("Todas las edades generadas:", edades)
+
+mayores_de_edad = [edad for edad in edades if edad >= 18]
+print("Edades mayores de edad:", mayores_de_edad)
+
+menores_de_edad = [edad for edad in edades if edad < 18]
+print("Edades mayores de edad:", menores_de_edad)
